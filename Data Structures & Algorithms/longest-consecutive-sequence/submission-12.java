6c75c3392eb48759348ede81ba806ca80926d830
Bryce Chao <brycechao27@gmail.com>
2026-09-01 21:50:47 -0700

    Add: longest-consecutive-sequence - submission-12

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-12.java
@@ -0,0 +1,48 @@
+// class Solution {
+//     public int longestConsecutive(int[] nums) {
+//         Set<Integer> set = new HashSet<>();
+
+//         int longest = 0;
+//         for (int num: nums){
+//             if(!set.contains(num)){
+//                 set.add(num);
+//             }
+//         }
+//         System.out.println(set);
+//         for (int i = 0; i < nums.length; i++){
+//             int longer = 1;
+//             //if we are at the smallest value consecutively
+//             if(!set.contains(nums[i]-1)){
+//                 int j = nums[i];
+//                 while(set.contains(j+1)){
+//                     longer++;
+//                     j++;
+//                     set.remove(j);
+//                 }
+//             }
+//             longest = Math.max(longer,longest);
+//         }
+        
+//         return longest;
+//     }
+// }
+public class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+        int longest = 0;
+
+        for (int num : numSet) {
+            if (!numSet.contains(num - 1)) {
+                int length = 1;
+                while (numSet.contains(num + length)) {
+                    length++;
+                }
+                longest = Math.max(longest, length);
+            }
+        }
+        return longest;
+    }
+}
