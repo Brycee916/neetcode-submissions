328767b54f993bbe433dafa9402a384324b9e77f
Bryce Chao <brycechao27@gmail.com>
2026-09-02 23:12:02 -0700

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length()-1;
+        s = s.toLowerCase();
+        System.out.println(s);
+        while (left < right){
+            if (!Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            else if (!Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+            else{//left and right are character or digit
+                if (s.charAt(left) != s.charAt(right)){
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
